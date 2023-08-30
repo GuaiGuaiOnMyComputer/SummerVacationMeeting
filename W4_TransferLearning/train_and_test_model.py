99f59497600923e6aba31a12a99b50d133fa4a96
eric <ericaccfin@gmai.com>
2023-08-30 14:30:04 +0800

    denestified train_model sucks. Include original train_model

--- W4_TransferLearning/train_and_test_model.py
+++ W4_TransferLearning/train_and_test_model.py
@@ -8,9 +8,80 @@ from torchvision.models import ResNet
 from torch.utils.data import DataLoader
 from tempfile import TemporaryDirectory
 
-def train_model(model:ResNet, loss_fn:CrossEntropyLoss, dataloaders:dict[str, DataLoader], dataset_size:dict['str', int], optimizer:optim, scheduler:lr_scheduler, num_epochs=25, device = "cuda") -> ResNet:
+
+def tutorial_train_model(model, criterion, optimizer, scheduler, dataloaders, dataset_sizes, num_epochs=25, device = "cuda"):
     since = time.time()
-    
+
+    # Create a temporary directory to save training checkpoints
+    with TemporaryDirectory() as tempdir:
+        best_model_params_path = join(tempdir, 'best_model_params.pt')
+
+        torch.save(model.state_dict(), best_model_params_path)
+        best_acc = 0.0
+
+        for epoch in range(num_epochs):
+            print(f'Epoch {epoch}/{num_epochs - 1}')
+            print('-' * 10)
+
+            # Each epoch has a training and validation phase
+            for phase in ['train', 'val']:
+                if phase == 'train':
+                    model.train()  # Set model to training mode
+                else:
+                    model.eval()   # Set model to evaluate mode
+
+                running_loss = 0.0
+                running_corrects = 0
+
+                # Iterate over data.
+                for inputs, labels in dataloaders[phase]:
+                    inputs = inputs.to(device)
+                    labels = labels.to(device)
+
+                    # zero the parameter gradients
+                    optimizer.zero_grad()
+
+                    # forward
+                    # track history if only in train
+                    with torch.set_grad_enabled(phase == 'train'):
+                        outputs = model(inputs)
+                        _, preds = torch.max(outputs, 1)
+                        loss = criterion(outputs, labels)
+
+                        # backward + optimize only if in training phase
+                        if phase == 'train':
+                            loss.backward()
+                            optimizer.step()
+
+                    # statistics
+                    running_loss += loss.item() * inputs.size(0)
+                    running_corrects += torch.sum(preds == labels.data)
+                if phase == 'train':
+                    scheduler.step()
+
+                epoch_loss = running_loss / dataset_sizes[phase]
+                epoch_acc = running_corrects.double() / dataset_sizes[phase]
+
+                print(f'{phase} Loss: {epoch_loss:.4f} Acc: {epoch_acc:.4f}')
+
+                # deep copy the model
+                if phase == 'val' and epoch_acc > best_acc:
+                    best_acc = epoch_acc
+                    torch.save(model.state_dict(), best_model_params_path)
+
+            print()
+
+        time_elapsed = time.time() - since
+        print(f'Training complete in {time_elapsed // 60:.0f}m {time_elapsed % 60:.0f}s')
+        print(f'Best val Acc: {best_acc:4f}')
+
+        # load best model weights
+        model.load_state_dict(torch.load(best_model_params_path))
+    return model
+ 
+
+def my_train_model(model:ResNet, loss_fn:CrossEntropyLoss, dataloaders:dict[str, DataLoader], dataset_size:dict['str', int], optimizer:optim, scheduler:lr_scheduler, num_epochs=25, device = "cuda") -> ResNet:
+    since = time.time()
     # Create a temporary directory to save training checkpoints
     with TemporaryDirectory() as tempdir:
         best_model_params_path = join(tempdir, 'best_model_params.pt')
@@ -20,7 +91,9 @@ def train_model(model:ResNet, loss_fn:CrossEntropyLoss, dataloaders:dict[str, Da
         model = model.to(device)
 
         for epoch in range(num_epochs):
-            print(f"Current at {epoch} epoch")
+            print(f'Epoch {epoch}/{num_epochs - 1}')
+            print('-' * 10)
+
             model.train()
             model, avg_train_acc, avg_train_loss = __model_fit(model, dataloaders['train'], dataset_size['train'], optimizer, scheduler, loss_fn, device)
             model.eval()
@@ -30,47 +103,48 @@ def train_model(model:ResNet, loss_fn:CrossEntropyLoss, dataloaders:dict[str, Da
                 torch.save(model.state_dict(), best_model_params_path)
                 best_validation_acc = avg_val_acc
 
+            print(f'Train Loss: {avg_train_loss:.4f} Acc: {avg_train_acc:.4f}')
+            print(f'Vali Loss : {avg_val_loss:.4f} Acc: {avg_val_acc:.4f}')
+
         time_elapsed = time.time() - since
         print(f'Training complete in {time_elapsed // 60:.0f}m {time_elapsed % 60:.0f}s')
-        print(f"Highest validation accuracy is {best_validation_acc}")
+        print(f"Highest validation accuracy is {best_validation_acc:.3f}")
 
         model.load_state_dict(torch.load(best_model_params_path))
-    return model
+    return model 
 
 def __model_fit(model:ResNet, train_dataloader:DataLoader, train_size:int, optimizer:optim, scheduler:lr_scheduler, loss_fn:CrossEntropyLoss, device:str) -> tuple[ResNet, float, float]:
 
-    avg_epoach_train_acc = torch.zeros(1, dtype = torch.float32, device = device)
+    avg_epoach_train_acc = torch.zeros(1, dtype = torch.float32, device = device, requires_grad = False)
     avg_epoach_train_loss = torch.zeros_like(avg_epoach_train_acc)
     for inputs, labels in train_dataloader:
+        optimizer.zero_grad()
         inputs = inputs.to(device)
         labels = labels.to(device)
-        batch_train_loss, batch_train_predict = __predict_and_get_batch_loss(model, inputs, labels, loss_fn, device)
+        batch_train_loss, batch_train_predict = __predict_and_get_batch_loss(model, inputs, labels, loss_fn)
         batch_train_loss.backward()
         optimizer.step()
-        scheduler.step()
-        optimizer.zero_grad()
 
-        avg_epoach_train_acc += torch.sum(batch_train_predict == labels)
-        avg_epoach_train_loss += batch_train_loss
+        avg_epoach_train_acc += torch.sum(batch_train_predict == labels.data)
+        avg_epoach_train_loss += batch_train_loss * inputs.size(0)
+        scheduler.step()
 
     return model, avg_epoach_train_acc.item() / train_size, avg_epoach_train_loss.item() / train_size
 
 def __model_validate(model:ResNet, val_dataloader:DataLoader, val_size:int, loss_fn:CrossEntropyLoss, device:str) -> tuple[float, float]:
-    avg_epoach_validation_acc = torch.zeros(1, dtype = torch.float32, device = device)
+    avg_epoach_validation_acc = torch.zeros(1, dtype = torch.float32, device = device, requires_grad = False)
     avg_epoach_validation_loss = torch.zeros_like(avg_epoach_validation_acc)
     with torch.no_grad():
         for inputs, labels in val_dataloader:
             inputs = inputs.to(device)
             labels = labels.to(device)
-            batch_val_loss, batch_val_predict = __predict_and_get_batch_loss(model, inputs, labels, loss_fn, device)
-            avg_epoach_validation_acc += torch.sum(batch_val_predict == labels)
+            batch_val_loss, batch_val_predict = __predict_and_get_batch_loss(model, inputs, labels, loss_fn)
+            avg_epoach_validation_acc += torch.sum(batch_val_predict == labels.data)
             avg_epoach_validation_loss += batch_val_loss
     return avg_epoach_validation_acc.item() / val_size, avg_epoach_validation_loss.item() / val_size
 
 
-def __predict_and_get_batch_loss(model:ResNet, inputs:torch.tensor, labels:torch.tensor, loss_fn:CrossEntropyLoss, device:str) -> tuple[torch.tensor, torch.tensor]:
-    # labels = labels.to(device)
-    # inputs = inputs.to(device)
+def __predict_and_get_batch_loss(model:ResNet, inputs:torch.tensor, labels:torch.tensor, loss_fn:CrossEntropyLoss) -> tuple[torch.tensor, torch.tensor]:
     outputs = model(inputs)
     _, predicts = torch.max(outputs, 1)
     loss = loss_fn(outputs, labels)
